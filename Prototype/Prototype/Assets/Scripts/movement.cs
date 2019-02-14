9a785fbac0ce38fd6f6b198599715beac6de5b72
Reorso <reorso@hotmail.it>
2019-02-14 18:06:06 +0000

    created a character prototype and script
    basic sprite for a character/enemy prototype and started to make a movement script prototype

---
+++ Prototype/Prototype/Assets/Scripts/movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+    public float speed = 5;
+    public Vector2 direction;
+    Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+        rb.velocity = direction * speed * Time.deltaTime;
+    }
+}
