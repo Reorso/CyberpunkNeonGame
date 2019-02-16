6d9b51af108b65e3d47984cf5c86475b87a3f49e
Reorso <reorso@hotmail.it>
2019-02-16 17:52:57 +0000

    added a shooting system

---
+++ Prototype/Prototype/Assets/Scripts/LaserSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserSpawner : MonoBehaviour {
+    public GameObject laserP;
+    GameObject temp;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Shoot();
+        }
+
+	}
+
+    void Shoot()
+    {
+        temp = Instantiate(laserP);
+        temp.transform.position = transform.position;
+        temp.transform.rotation = transform.rotation;
+    }
+}
--- Prototype/Prototype/Assets/Scripts/movement.cs
+++ Prototype/Prototype/Assets/Scripts/movement.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class movement : MonoBehaviour {
     public float speed = 5;
-    public Vector2 direction;
+    public Vector2 direction, rot;
+    Vector3 mpos;
     Rigidbody2D rb;
+    Transform tr;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+        tr = GetComponent<Transform>();
 	}
 
     // Update is called once per frame
@@ -16,5 +19,12 @@ public class movement : MonoBehaviour {
         direction.x = Input.GetAxis("Horizontal");
         direction.y = Input.GetAxis("Vertical");
         rb.velocity = direction * speed * Time.deltaTime;
+
+        mpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        rot = new Vector2(
+            mpos.x - tr.position.x, 
+            mpos.y - tr.position.y
+            );
+        tr.up = rot;
     }
 }
