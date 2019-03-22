aeee661766fa24999522700fe304e80317eab87b
Reorso <reorso@hotmail.it>
2019-03-22 03:17:58 +0000

    added first character power
    needs to be polished tho

---
+++ Prototype(LevelGen)/Assets/Scripts/Player/RedCode.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedCode : MonoBehaviour {
+    private List<GameObject> clones;
+    public GameObject bullet, clone;
+    public float coolDown;
+    private float time;
+
+	// Use this for initialization
+	void Start () {
+        time = 0;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(time < coolDown)
+        {
+            time += Time.deltaTime;
+        }
+		else if(Input.GetAxis("Power") > 0)
+        {
+            ShootSpecial();
+            time = 0;
+        }
+	}
+
+    void ShootSpecial()
+    {
+        GameObject temp = Instantiate(bullet, this.transform);
+        temp.transform.localPosition = Vector3.zero;
+        temp.transform.localRotation = Quaternion.identity;
+
+    }
+
+    public void AddClone(Vector3 pos)
+    {
+        clones.Add(Instantiate(clone, pos, Quaternion.identity));
+    }
+}
+
---
+++ Prototype(LevelGen)/Assets/Scripts/Projectiles/RedCodeBullet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedCodeBullet : MonoBehaviour {
+
+    public float speed;
+    public Vector2 initialpos;
+    public float maxDist;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = transform.up * speed;
+        initialpos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(initialpos, transform.position) >= maxDist)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player"))
+        {
+            print("hit");
+            Destroy(this.gameObject);
+        }
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            GetComponentInParent<RedCode>().AddClone(transform.position);
+        }
+    }
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Enemy"))
+    //    {
+    //        GetComponentInParent<RedCode>().AddClone(transform.position);
+    //    }
+    //}
+}
